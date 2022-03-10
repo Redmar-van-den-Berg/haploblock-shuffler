640b70d00fe4cbe57805f04e3c107da208f56828
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-03-10 10:26:32 +0100

    Refactor, only compare pairs of calls

--- src/python_project/utils.py
+++ src/python_project/utils.py
@@ -18,20 +18,26 @@ def is_homozygous(call):
     return call['GT'][0] == call['GT'][1]
 
 
-def compatible(calls, call):
+def is_heterozygous(call):
+    """Determine if a call is heterozygous"""
+    return not is_homozygous(call)
+
+
+def is_compatible(call1, call2):
+    """Are two calls compatible"""
+    # If both are phased, the phase set must match.
+    if 'PS' in call1 and 'PS' in call2:
+        return call1['PS'] == call2['PS']
+
+    # Two homozygous calls are compatible
+    if is_homozygous(call1) or is_homozygous(call2):
+        return True
+
+    # Two heterozygous calls are not compatibhle
+    if is_heterozygous(call1) and is_heterozygous(call2):
+        return False
+
+
+def are_compatible(calls, call):
     """ Determine if a call is compatible with a list of calls """
-    # Any variant is compatible with an empty list of cals
-    if not calls:
-        return True
-
-    # A homozygous call is compatible with any list of calls
-    if is_homozygous(call):
-        return True
-
-    # Check each call in turn, does the phase set match for any call
-    for c in calls:
-        if 'PS' in call:
-            return call['PS'] == c.get('PS')
-
-    # If call is heterozygous, all (other) existing calls must be homozygous
-    return all((is_homozygous(c) for c in calls))
+    return all((is_compatible(call, c) for c in calls))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,37 +17,52 @@ def test_is_hom_extreme():
     assert utils.is_homozygous({'GT': (99, 99)})
 
 
-def test_compatible_empty():
+def test_are_compatible_empty():
     """ Every call is compatible with an empty list """
-    assert utils.compatible([], None)
+    assert utils.are_compatible([], None)
 
 
-def test_compatible_hom():
-    """ A homozygous variant is compatible with any list of variants """
-    assert utils.compatible([1], {'GT': (1, 1)})
+def test_is_compatible_hom():
+    """ A homozygous variant is compatible with any other variant """
+    call = {'GT': (1, 1)}
+    assert utils.is_compatible(call, call) is True
 
 
-def test_compatible_two_het():
+def test_is_compatible_two_het():
     """Two heterozygous calls are not compatible """
     call = {'GT': (0, 1)}
-    assert utils.compatible([call], call) is False
+    assert utils.is_compatible(call, call) is False
 
 
-def test_compatible_hom_het():
+def test_is_compatible_hom_het():
     """A homozygous and heterozygous call are compatible"""
     hom = {'GT': (0, 0)}
     het = {'GT': (0, 1)}
-    assert utils.compatible([hom], het)
+    assert utils.is_compatible(hom, het)
 
 
-def test_compatible_phase_set():
+def test_is_compatible_phase_set():
     """Two heterozygous variants from the same phase set are compatible"""
     call = {'GT': (0, 1), 'PS': 1}
-    assert utils.compatible([call], call)
+    assert utils.is_compatible(call, call)
 
 
-def test_incompatible_phase_set():
+def test_is_compatible_incompatible_phase_set():
     """Two conflicting phase sets are not compatible"""
     call1 = {'GT': (0, 1), 'PS': 1}
     call2 = {'GT': (0, 1), 'PS': 2}
-    assert utils.compatible([call1], call2) is False
+    assert utils.is_compatible(call1, call2) is False
+
+
+def test_is_compatible_phased_hom():
+    """A call with phase set is compatible with a homozygous call"""
+    hom = {'GT': (0, 0)}
+    call = {'GT': (0, 1), 'PS': 1}
+    assert utils.is_compatible(hom, call)
+
+
+def test_is_compatible_incompatible_phased_hom():
+    """A homozygous call with an incompatible phase set is not compatible"""
+    hom = {'GT': (0, 0), 'PS': 0}
+    call = {'GT': (0, 1), 'PS': 1}
+    assert utils.is_compatible(hom, call) is False
