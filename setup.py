610f8419ccdf6d9a09284b8d1efad24509bcdd5b
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-03-11 08:34:42 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.* !=3.9.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.9.*",
     install_requires=[
         "pyvcf == 0.6.8"
     ],
