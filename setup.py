5f9114200a9bd14c5650029653f6882874e63442
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-03-11 08:12:45 +0100

    Run black

--- setup.py
+++ setup.py
@@ -17,52 +17,51 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='haploblock-shuffler',
-    version='0.0.1',
-    license='MIT',
-    description='Shuffle phased and unphased blocks',
-    long_description='Create all possible combinations of phased blocks in a vcf',
-    author='Redmar van den Berg',
-    author_email='RedmarvandenBerg@lumc.nl',
-    url='https://github.com/redmar-van-den-berg/haploblock-shuffler',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    name="haploblock-shuffler",
+    version="0.0.1",
+    license="MIT",
+    description="Shuffle phased and unphased variants",
+    long_description="Create all possible combinations of phased and unphased blocks in a vcf",
+    author="Redmar van den Berg",
+    author_email="RedmarvandenBerg@lumc.nl",
+    url="https://github.com/redmar-van-den-berg/haploblock-shuffler",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Changelog': 'https://github.com/redmar-van-den-berg/haploblock-shuffler/blob/master/CHANGELOG.rst',
-        'Issue Tracker': 'https://github.com/redmar-van-den-berg/haploblock-shuffler/issues',
+        "Changelog": "https://github.com/redmar-van-den-berg/haploblock-shuffler/blob/master/CHANGELOG.rst",
+        "Issue Tracker": "https://github.com/redmar-van-den-berg/haploblock-shuffler/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
@@ -71,12 +70,8 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    setup_requires=[
-        'pytest-runner',
-    ],
+    setup_requires=["pytest-runner"],
     entry_points={
-        'console_scripts': [
-            'haploblock-shuffler=haploblock_shuffler.cli:main',
-        ]
+        "console_scripts": ["haploblock-shuffler=haploblock_shuffler.cli:main"]
     },
 )
