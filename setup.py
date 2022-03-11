1ead8948b70626ba6b095b39dcdc548c00812a03
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-03-11 08:20:59 +0100

    Add pyvcf dependency

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        "pyvcf == 0.6.8"
     ],
     extras_require={
         # eg:
